49b385424c1a4173c6d1cb290c691c4538a61c10
Ankur Srivastava <best.ankur@gmail.com>
2019-11-12 16:24:42 +0100

    included proper download url in setup.py
    Signed-off-by: Ankur Srivastava <best.ankur@gmail.com>

--- internal/python/setuppytext.go
+++ internal/python/setuppytext.go
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2019 Ankur Srivastava
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package python
 
 var setupyText = `import re
@@ -23,13 +45,17 @@ def get_version():
 install_requires = ['future']
 
 
-test_requires = ['pytest', 'pytest-sugar', 'pytest-asyncio', 'pytest-cov', ]
+TEST_REQUIRES = [
+    "pytest==5.2.1",
+    "pytest-sugar==0.9.2",
+    "pytest-asyncio==0.10.0",
+    "pytest-cov==2.8.1",
+]
 
 
-extras_require = {
-    'dev': [
-        '',
-    ]
+EXTRAS_REQUIRE = {
+    "dev": ["python-language-server[all]", "black==19.3b0", "isort"],
+    "test": TEST_REQUIRES,
 }
 
 
@@ -37,17 +63,18 @@ setup(
     name='{{.appWithHyphen}}',
     description="Some description about your project",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     version=get_version(),
     include_package_data=True,
     install_requires=install_requires,
     setup_requires=['pytest-runner'],
-    extras_require=extras_require,
-    tests_require=test_requires,
+    extras_require=EXTRAS_REQUIRE,
+    tests_require=TEST_REQUIRES,
     packages=find_packages(),
     zip_safe=False,
     author="{{.author}}",
     author_email = "{{.authoremail}}",
-    download_url="{{.scm}}/{{.scmusername}}/{}.tar.gz".format(get_version()),
+    download_url="https://{{.scm}}/{{.scmusername}}/{{.appWithHyphen}}/{}.tar.gz".format(get_version()),
     classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
